fb282f3e400914206864a8bac06f4db8b19e2ffa
free-math-site <georgemakris2005@yahoo.com>
2026-02-16 16:27:17 +0200

    Delete back-to-top.v1.js

--- back-to-top.v1.js
+++
@@ -1,9 +0,0 @@
-{
-const a=document.getElementById("back-to-top");
-function updateBackToTopVisibility(){
-if(document.querySelector("main").getBoundingClientRect().top>=0 || document.querySelector("footer").getBoundingClientRect().top<=window.innerHeight){a.classList.add("hidden");}
-else{a.classList.remove("hidden");}
-};
-window.addEventListener("scroll",updateBackToTopVisibility);
-updateBackToTopVisibility();
-}
